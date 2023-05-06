40ed39b717a05f81c6a01076ad36be1898b7fc2c
Omar Hamid <omarhamid664@gmail.com>
2023-05-05 20:25:55 -0600

    Add user auth feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,11 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
   def enhanced_cart
     @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
   end
